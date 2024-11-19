a84adb5b45a215f6838cfbd3a97b40ad747cf1f6
Maryia Samitava <mashasamitova1511@gmail.com>
2024-11-19 19:02:41 +0200

    fix: fixed hundredAfterOdd.js

--- src/hundredAfterOdd.js
+++ src/hundredAfterOdd.js
@@ -6,7 +6,7 @@
 module.exports.hundredAfterOdd = function hundredAfterOdd(arr) {
   for (let i = arr.length - 1; i >= 0; i--) {
     if (Math.abs(arr[i]) % 2 === 1) {
-      arr.splice(i + 1, 0, 100); // Вставляем после текущего элемента
+      arr.splice(i + 1, 0, 100); 
     }
   }
   return arr;
