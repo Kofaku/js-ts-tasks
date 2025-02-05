36cf72a0eeba14f610f5d718c7a702521e3db3e4
Maryia Samitava <mashasamitova1511@gmail.com>
2025-02-05 22:24:06 +0300

    feat: solution hometasks-08

--- src/customCache.ts
+++ src/customCache.ts
@@ -1,3 +1,38 @@
 export default class CustomCache<K, V> {
-  // your code here
+  private maxSize: number;
+
+  private cache: Map<K, V>;
+
+  constructor(maxSize: number) {
+    this.maxSize = maxSize;
+    this.cache = new Map<K, V>();
+  }
+
+  set(key: K, value: V): void {
+    if (this.cache.size >= this.maxSize) {
+      const firstKey = this.cache.keys().next().value;
+      if (firstKey !== undefined) {
+        this.cache.delete(firstKey);
+      }
+    }
+    this.cache.set(key, value);
+  }
+
+  get(key: K): V | undefined {
+    if (this.cache.has(key)) {
+      const value = this.cache.get(key)!;
+      this.cache.delete(key);
+      this.cache.set(key, value);
+      return value;
+    }
+    return undefined;
+  }
+
+  size(): number {
+    return this.cache.size;
+  }
+
+  clear(): void {
+    this.cache.clear();
+  }
 }
