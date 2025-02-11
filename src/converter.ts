c0f0ac70e7c540006567b1df2061cad8644f5c2e
Maryia Samitava <mashasamitova1511@gmail.com>
2025-02-11 20:02:43 +0300

    fix: fixed

--- src/converter.ts
+++ src/converter.ts
@@ -8,19 +8,22 @@
 module.exports.converter = function (value: number, from: string, to: string): number {
   if (from === 'm' && to === 'mi') {
     return parseFloat((value * 0.000621371).toFixed(2));
-  } else if (from === 'mi' && to === 'm') {
-    return parseFloat((value * 1609.34).toFixed(2));
+  }
+  if (from === 'mi' && to === 'm') {
+    return parseFloat((value * 1609.344).toFixed(2));
   }
 
   if (from === 'gr' && to === 'pound') {
     return parseFloat((value * 0.00220462).toFixed(2));
-  } else if (from === 'pound' && to === 'gr') {
+  }
+  if (from === 'pound' && to === 'gr') {
     return parseFloat((value * 453.592).toFixed(2));
   }
 
   if (from === 'C' && to === 'K') {
     return parseFloat((value + 273.15).toFixed(2));
-  } else if (from === 'K' && to === 'C') {
+  }
+  if (from === 'K' && to === 'C') {
     return parseFloat((value - 273.15).toFixed(2));
   }
 
